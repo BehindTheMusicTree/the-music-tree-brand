108d9c8d3f02d9527c9c8830655a9809397ee956
mignot <garcia.andreas.1991@gmail.com>
2026-06-08 16:33:06 +0200

    add packages/brand/src/index.ts

---
+++ packages/brand/src/index.ts
@@ -0,0 +1,35 @@
+export {
+  GithubSponsorButton,
+  type GithubSponsorButtonProps,
+  GithubSponsorButtonUrl,
+  BtmtSponsorButton,
+  ORG_GITHUB_SPONSOR_BUTTON_URL,
+  type BtmtSponsorButtonProps,
+  Button,
+  type ButtonProps,
+  TheMusicTreeByline,
+  TheMusicTreeHorizontalLink,
+  TheMusicTreeMarkLink,
+  ORG_DOMAIN,
+  ORG_NAME,
+  HTMT_API_GITHUB_URL,
+  ORG_GITHUB_URL,
+  HTMT_FRONT_SUBDOMAIN,
+  HTMT_API_SUBDOMAIN,
+  GTMT_FRONT_SUBDOMAIN,
+  GTMT_API_SUBDOMAIN,
+  AUDIOMETA_FRONT_SUBDOMAIN,
+  AUDIOMETA_API_SUBDOMAIN,
+  TMTA_SUBDOMAIN,
+  TMD_GAME_SUBDOMAIN,
+  TMD_SHOWCASE_SUBDOMAIN,
+  parseOrgSiteHref,
+  resolveOrgSiteHref,
+  type TheMusicTreeBylineProps,
+  type TheMusicTreeHorizontalLinkProps,
+  type TheMusicTreeMarkLinkProps,
+} from "./components";
+export * from "./tokens";
+export * from "./hooks";
+export * from "./utils";
+export * from "./brand";
